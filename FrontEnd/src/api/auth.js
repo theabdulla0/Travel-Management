70d23e3c284362237ae3cb59bbfdf384a8afda6c
Abdulla <pikasu2000@gmail.com>
2025-10-08 16:22:20 +0530

    update

--- FrontEnd/src/api/auth.js
+++ FrontEnd/src/api/auth.js
@@ -1,50 +1,7 @@
 import axios from "axios";
-// import { store } from "../app/store";
-import { logout } from "../features/auth/authThunk";
-
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/auth`,
   withCredentials: true,
 });
-
-let isRefreshing = false;
-let failedQueue = [];
-
-const processQueue = (error) => {
-  failedQueue.forEach(({ reject }) => reject(error));
-  failedQueue = [];
-};
-
-API.interceptors.response.use(
-  (res) => res,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      if (isRefreshing) {
-        return new Promise((_, reject) => {
-          failedQueue.push({ reject });
-        });
-      }
-
-      originalRequest._retry = true;
-      isRefreshing = true;
-
-      try {
-        await API.post("/refresh-token");
-        isRefreshing = false;
-        return API(originalRequest);
-      } catch (refreshError) {
-        dispatch(logout());
-        window.location.href = "/";
-        isRefreshing = false;
-        processQueue(refreshError);
-        return Promise.reject(refreshError);
-      }
-    }
-
-    return Promise.reject(error);
-  }
-);
 
 export default API;
